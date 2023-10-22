731e2281e58b32aa53563adbd7fa550e09f511d6
Marek Maškarinec <marek@mrms.cz>
2023-10-22 21:01:35 +0200

    Add search

---
+++ pak/search.py
@@ -0,0 +1,20 @@
+
+import argparse
+import requests
+
+import common
+
+
+def search(args):
+    par = argparse.ArgumentParser(
+        prog='search', description='Search for a package')
+    par.add_argument('query', help='Query string')
+
+    ns = par.parse_args(args)
+
+    resp = requests.get(f"{common.url}api/search/{ns.query}")
+    # decode the result from json
+    result = resp.json()
+
+    for r in result:
+        print(f"{r.get('name')} - {r.get('description')}")
