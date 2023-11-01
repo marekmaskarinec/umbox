9447aa7501106fdc8995088a0c51833451218c69
Vasiliy Tereshkov <vtereshkov@mail.ru>
2023-11-01 09:50:05 +0100

    run: Add dependencies to PATH

--- pak/run.py
+++ pak/run.py
@@ -9,11 +9,21 @@ def run(args):
 
     cmd = meta.get('run')
 
+    path_sep = ''
+
     if os.name == 'posix' and 'run_posix' in meta:
         cmd = meta.get('run_posix')
+        path_sep = ':'
 
     if os.name == 'nt' and 'run_windows' in meta:
         cmd = meta.get('run_windows')
+        path_sep = ';'
+        
+    path = os.environ['PATH']
+    deps = meta.get('dependencies')
+    for dep in deps:
+        path = os.getcwd() + '/pak/' + dep + path_sep + path
+    os.environ['PATH'] = path 
 
     cmd = cmd.split(' ') + args
     try:
