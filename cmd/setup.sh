da23144afc9970ab3f13ebf5093e47679d227b0e
Marek Maškarinec <marek@mrms.cz>
2024-04-08 09:30:36 +0200

    setup.sh changes
    Signed-off-by: Marek Maškarinec <marek@mrms.cz>

--- cmd/setup.sh
+++ cmd/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+mkdir -p ~/.local/opt
 cd ~/.local/opt
-curl -L https://umbox.tophat2d.dev/dl/umbox_portable.zip -O
-unzip umbox_portable.zip
-mv umbox_portable umbox
+curl -L https://umbox.tophat2d.dev/dl/umbox_linux.zip -O
+unzip umbox_linux.zip
+mv umbox_linux umbox
 export PATH=$PATH:~/.local/opt/umbox/bin
-rm umbox_portable.zip
+rm umbox_linux.zip
