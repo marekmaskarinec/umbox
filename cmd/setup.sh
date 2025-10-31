a9fac7a39bc13c684988f066e0cf4ce70acf9f64
Marek Maškarinec <marek@mrms.cz>
2025-10-31 12:26:38 +0100

    cmd/setup.sh: fixes
    Signed-off-by: Marek Maškarinec <marek@mrms.cz>

--- cmd/setup.sh
+++ cmd/setup.sh
@@ -2,8 +2,10 @@
 
 mkdir -p ~/.local/opt
 cd ~/.local/opt
+rm -rf umbox
 curl -L https://umbox.tophat2d.dev/dl/umbox_linux.zip -O
-unzip umbox_linux.zip
+unzip -q umbox_linux.zip
 mv umbox_linux umbox
-export PATH=$PATH:~/.local/opt/umbox/bin
+echo "$PATH" | grep "$HOME/.local/opt/umbox" || echo NOTE: Please make sure ~/.local/opt/umbox is in your PATH
+export PATH="$PATH:~/.local/opt/umbox"
 rm umbox_linux.zip
